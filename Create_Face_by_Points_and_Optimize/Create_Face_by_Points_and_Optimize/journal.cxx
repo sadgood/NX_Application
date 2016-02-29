84cc4a324949d7c822e3bde1ed165dd9651ec7be
Ziyun Zhao <zhaoziy@hotmail.com>
2016-02-29 15:50:16 +0800

    new

--- Create_Face_by_Points_and_Optimize/Create_Face_by_Points_and_Optimize/journal.cxx
+++ Create_Face_by_Points_and_Optimize/Create_Face_by_Points_and_Optimize/journal.cxx
@@ -1,6 +1,3 @@
-// NX 8.5.3.3
-// Journal created by Zhao on Mon Feb 29 14:10:55 2016 中国标准时间
-//
 #include <uf_defs.h>
 #include <NXOpen/NXException.hxx>
 #include <NXOpen/Session.hxx>
@@ -23,12 +20,7 @@
 #include <NXOpen/UnitCollection.hxx>
 using namespace NXOpen;
 
-extern "C" DllExport int ufusr_ask_unload()
-{
-    return (int)Session::LibraryUnloadOptionImmediately;
-}
-
-extern "C" DllExport void ufusr(char *param, int *retCode, int paramLen)
+void CreatePoints()
 {
     Session *theSession = Session::GetSession();
     Part *workPart(theSession->Parts()->Work());
@@ -42,25 +34,4 @@ extern "C" DllExport void ufusr(char *param, int *retCode, int paramLen)
     point1 = workPart->Points()->CreatePoint(coordinates1);
     
     point1->SetVisibility(SmartObject::VisibilityOptionVisible);
-    
-    Features::Feature *nullFeatures_Feature(NULL);
-    
-    if ( !workPart->Preferences()->Modeling()->GetHistoryMode() )
-    {
-        throw NXException::Create("Create or edit of a Feature was recorded in History Mode but playback is in History-Free Mode.");
-    }
-    
-    Features::PointFeatureBuilder *pointFeatureBuilder1;
-    pointFeatureBuilder1 = workPart->BaseFeatures()->CreatePointFeatureBuilder(nullFeatures_Feature);
-    
-    pointFeatureBuilder1->SetPoint(point1);
-    
-    NXObject *nXObject1;
-    nXObject1 = pointFeatureBuilder1->Commit();
-    
-    pointFeatureBuilder1->Destroy();
-    
-    // ----------------------------------------------
-    //   Menu: Tools->Journal->Stop Recording
-    // ----------------------------------------------
 }
